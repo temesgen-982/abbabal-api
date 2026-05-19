459962f320641fc449161b67ff440569cc227e70
temesgen-982 <tedenadane@gmail.com>
2026-05-19 14:27:13 +0300

    fix: localhost origin url

--- apps/api/src/setup-app.ts
+++ apps/api/src/setup-app.ts
@@ -2,7 +2,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 export function configureApp(app: INestApplication) {
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173' || 'http://localhost' || 'capacitor://localhost',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173' || 'https://localhost' || 'capacitor://localhost',
     credentials: true,
   });
   app.useGlobalPipes(
